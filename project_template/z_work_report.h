f1bbbd145bb804d2e5c666edad032ee005695ac4
jason w <jayywong92@gmail.com>
2019-02-25 11:06:50 -0800

    rearranging stuff

---
+++ project_template/z_work_report.h
@@ -0,0 +1,16 @@
+#ifndef Z_WORK_REPORT_H
+#define Z_WORK_REPORT_H
+/*
+    Features:
+        -Not Implemented:
+
+        -Implemented:
+
+        -Partly implemented:
+
+    Bugs:
+
+    Reflections:
+
+*/
+#endif // Z_WORK_REPORT_H
