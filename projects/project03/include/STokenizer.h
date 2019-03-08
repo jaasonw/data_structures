93611614e8c8d101fe2e7dedbaf765b5f75a7b1c
jason w <jayywong92@gmail.com>
2019-03-08 08:46:22 -0800

    public constant

--- projects/project03/include/STokenizer.h
+++ projects/project03/include/STokenizer.h
@@ -4,11 +4,24 @@
 #include <cstring>
 
 class STokenizer {
+public:
+    STokenizer();
+    STokenizer(char* str);
+    bool done() { return pos >= strlen(buffer); }
+    bool fail() { return state == -1; };
+
+    // extract one token (very similar to the way cin >> works)
+    friend STokenizer& operator>>(STokenizer& s, Token& t);
+
+    // set a new string as the input string
+    void set_string(char* str);
+
+    static const int MAX_BUFFER = 255;
+
 private:
     // constants
     static const int MAX_COLUMNS = state_machine::MAX_COLUMNS;
     static const int MAX_ROWS = state_machine::NUM_ROWS;
-    static const int MAX_BUFFER = 255;
 
     // state table
     int state_table[MAX_ROWS][MAX_COLUMNS];
@@ -29,16 +42,4 @@ private:
     // extract the longest string that match
     // one of the acceptable token types
     bool get_token(int start_state, std::string& token);
-
-public:
-    STokenizer();
-    STokenizer(char* str);
-    bool done() { return pos >= strlen(buffer); }
-    bool fail() { return state == -1; };
-
-    // extract one token (very similar to the way cin >> works)
-    friend STokenizer& operator>>(STokenizer& s, Token& t);
-
-    // set a new string as the input string
-    void set_string(char* str);
 };
