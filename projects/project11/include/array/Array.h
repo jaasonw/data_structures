ae6b5ca3ec30b8f9f90a7a65b53144278e15f3ed
jason w <jayywong92@gmail.com>
2019-05-14 11:38:42 -0700

    prints

--- projects/project11/include/array/Array.h
+++ projects/project11/include/array/Array.h
@@ -47,7 +47,78 @@ T& Array<T>::operator[](size_t index) const {
     if (index >= capacity || index < 0)
         throw std::out_of_range("Out of range");
     else
-        return data[index];
+#pragma once
+#include <iostream>
+#include <stdexcept>
+            // it's like an array, but it has bounds checking
+            template < typename T > class Array {
+        private:
+            T* data = nullptr;
+            size_t capacity;
+
+        public:
+            Array() : data() {}
+            Array(size_t size);
+            Array(const Array<T>& other);
+            ~Array() { delete[] data; }
+            Array<T>& operator=(const Array<T>& other);
+
+            // bounds checked element access, throws std::out_of_range
+            T& operator[](size_t index) const;
+
+            // copy src[] into self
+            void copy_from_array(const T* src, size_t src_size);
+
+            // prints the entire array
+            friend std::ostream& operator<<(std::ostream& outs,
+                                            const Array<T>& a) {
+                outs << '[' << ' ';
+                for (size_t i = 0; i < a.capacity; i++) {
+                    outs << a[i] << " ";
+                }
+                outs << ' ' << ']';
+                return outs;
+            }
+        };
+
+    template <typename T>
+    Array<T>::Array(size_t size) {
+        capacity = size;
+        data = new T[capacity]();
+    }
+    template <typename T>
+    Array<T>& Array<T>::operator=(const Array<T>& other) {
+        if (this == &other)
+            return *this;
+        delete[] data;
+        data = new T[other.capacity];
+        copy_from_array(other.data, other.capacity);
+        return *this;
+    }
+    template <typename T>
+    Array<T>::Array(const Array<T>& other) {
+        data = new T[other.capacity];
+        copy_from_array(other.data, other.capacity);
+    }
+
+    template <typename T>
+    T& Array<T>::operator[](size_t index) const {
+        if (index >= capacity || index < 0)
+            throw std::out_of_range("Out of range");
+        else
+            return data[index];
+    }
+
+    template <typename T>
+    void Array<T>::copy_from_array(const T* src, size_t src_size) {
+        capacity = src_size;
+        delete[] data;
+        data = new T[capacity]();
+        for (size_t i = 0; i < src_size; i++) {
+            data[i] = src[i];
+        }
+    }
+    return data[index];
 }
 
 template <typename T>
